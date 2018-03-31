f0ef6d5d02f0f24f8c91028116acdf2755e4196d
unknown <domanskah7777@gmail.com>
2018-03-31 21:57:00 +0300

    HW1

---
+++ HW_1/task1.py
@@ -0,0 +1,7 @@
+name = raw_input("What is your name?\n")
+age = raw_input("How old are you?\n")
+location = raw_input("Where are you live?\n")
+
+print "Hello, " + name + "!"
+print "Your age is " + age + "."
+print "You live in " + location + "!"
---
+++ HW_1/task2.py
@@ -0,0 +1,13 @@
+from __future__ import division
+from math import sin, exp, log
+
+a = input("What's value of a?\n")
+b = input("What's value of b?\n")
+print "a+b=" + str(a+b)
+print "a*b=" + str(a*b)
+print "a-b=" + str(a-b)
+print "a/b=" + str(a/b)
+print "a^b=" + str(a**b)
+print "sin(a) = " + str(sin(a))
+print "exp(b) = " + str(exp(b))
+print "log a(b) = " + str(log(b, a))
