fbd0df3beb19bf52a8765d6ba319ed80d915236e
Radbull123 <dmyterkotaras91@gmail.com>
2018-04-01 19:30:05 +0300

    My Home task

---
+++ HT1/task1.py
@@ -0,0 +1,9 @@
+name = raw_input('Enter Your name: ')
+
+age = int(raw_input('How old are you? '))
+
+live = raw_input('Where are you live?')
+
+print 'Hello, %s' %name
+print 'Your age is %d' %age
+print 'You live in %s' %live
