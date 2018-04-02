763b5cb1331cf106dc57e3a672b4d77874e9461e
Rostikst <delfin4uk@gmail.com>
2018-04-02 23:28:36 +0300

    HW1

--- HW1/1.py
+++ HW1/1.py
@@ -1,9 +1,9 @@
 #1st HW
-name = raw_input("What is your name?\n") #Ввід даних
-age  = raw_input("How old are you?\n")   #Ввід даних 
+name = raw_input("What is your name?\n")
+age  = raw_input("How old are you?\n")
 place_of_living = raw_input("Where do you live?\n")
-<<<<<<< HEAD
+
 print "Hello " + name + "!",
 print "Your age is " + age + "!",
 print "You live in " + place_of_living + "!"
-print "End"!
+
