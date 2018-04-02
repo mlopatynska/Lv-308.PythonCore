79fa66fe829932f182e710e58595ea9c658c8da6
Rostikst <delfin4uk@gmail.com>
2018-04-02 23:08:11 +0300

    was added dir HW1

---
+++ HW1/1.py
@@ -0,0 +1,14 @@
+#1st HW
+name = raw_input("What is your name?\n") #Ввід даних
+age  = raw_input("How old are you?\n")   #Ввід даних 
+place_of_living = raw_input("Where are you live?\n")
+<<<<<<< HEAD
+print "Hello " + name + "!",
+print "Your age is " + age + "!",
+print "You live in " + place_of_living + "!"
+print "End"!
+=======
+print "Hello " + name + "!"
+print "Your age is " + age + " and",
+print "you live in " + place_of_living + "!"
+>>>>>>> master
