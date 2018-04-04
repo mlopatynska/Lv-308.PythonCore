80a882dddd188c6eec1c1a4bc140b19e1c3c2163
Radbull123 <dmyterkotaras91@gmail.com>
2018-04-05 00:32:30 +0300

    Task2_02_3

---
+++ HT2/Task_2_2(3).py
@@ -0,0 +1,9 @@
+
+var1 = int(raw_input("Please enter any number: "))
+var2 = int(raw_input("Enter the second number: "))
+print var1, var2
+# Here you typing some number
+
+var1, var2 = var2, var1
+print var1, var2
+# In here we swap ours variables
