7cdb3b618b23cc666dd8b08be9c5a38399119d34
Radbull123 <dmyterkotaras91@gmail.com>
2018-04-05 00:54:29 +0300

    changed raw_input/ input

--- HT2/Task_2_2(3).py
+++ HT2/Task_2_2(3).py
@@ -1,6 +1,6 @@
 
-var1 = int(raw_input("Please enter any number: "))
-var2 = int(raw_input("Enter the second number: "))
+var1 = input("Please enter any number: ")
+var2 = input("Enter the second number: ")
 print var1, var2
 # Here you typing some number
 
