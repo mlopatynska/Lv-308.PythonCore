bd8c5432c40ad892dce70af7edcbfd8c0aedac9c
rromanitc <romaniv01@gmail.com>
2018-04-04 19:04:18 +0300

    add .gitignore file add requirements.txt file remove .idea folder modifier HM02.py

--- HM02.py
+++ HM02.py
@@ -1,13 +1,13 @@
 import math
 
-
 a = int(input("Please enter a variable: "))
 b = int(input("Please enter b variable: "))
 
 sum = a + b
 minus = a - b
-division = a/b
-multiplication = a*b
+division = a / b
+
+multiplication = a * b
 
 print("Sum of numbers are: " + str(sum))
 print("Minus of numbers are: " + str(minus))
@@ -16,14 +16,16 @@ print("Multiplication of numbers are: " + str(multiplication))
 print("Factorial of number a is: " + str(math.factorial(a)))
 print("Factorial of number b is: " + str(math.factorial(b)))
 
+
 def res(first, second, op):
-    if op=='+':
-        print(first+second)
-    if op=='-':
-        print(first-second)
-    if op=='/':
-        print(first/second)
-    if op=='*':
-        print(first*second)
+    if op == '+':
+        print(first + second)
+    if op == '-':
+        print(first - second)
+    if op == '/':
+        print(first / second)
+    if op == '*':
+        print(first * second)
+
 
 res(a, b, '-')
