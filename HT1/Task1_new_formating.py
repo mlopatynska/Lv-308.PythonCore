3dbd1bc4b7fb155887b70643daa9f088e37829d0
Radbull123 <dmyterkotaras91@gmail.com>
2018-04-05 13:11:00 +0300

    I going to rechange task

--- HT1/Task1_new_formating.py
+++
@@ -1,7 +0,0 @@
-name = raw_input('Enter Your name: ')
-
-age = int(raw_input('How old are you? '))
-
-live = raw_input('Where are you live? ')
-
-print' Hello {}\n Your age is {}\n You live in {}'.format(name, age, live)
