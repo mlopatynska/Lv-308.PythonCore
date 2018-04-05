59c08daa071f41e96f6e5dc553447ecb276cc21c
Radbull123 <dmyterkotaras91@gmail.com>
2018-04-05 11:51:11 +0300

    Rewrite Task2_2(3)

---
+++ HT2/Task_2_2(3).py
@@ -0,0 +1,9 @@
+
+var1 = input("Please enter any number: ")
+var2 = input("Enter the second number: ")
+print var1, var2
+# Here you typing some number
+
+var1, var2 = var2, var1
+print var1, var2
+# In here we swap ours variables
