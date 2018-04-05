edc5bfe28036fca163ff883e3103198f6b8267cf
Wirtos_new/Tep4ekeMI <game999888@gmail.com>
2018-04-05 07:01:21 +0300

    Updated_hw1_format

--- HomeWorks/HW_01_1.py
+++ HomeWorks/HW_01_1.py
@@ -6,4 +6,4 @@ n = "\n"
 name = str(unicode(raw_input("What is your name? ")))
 years = str(unicode(raw_input("How old are you? ")))
 city = str(unicode(raw_input( "Where are you live? ")))
-print "Hello, ",name, n, "Your age is ",years, n, "You live in ",city
+print "Hello, {0}\nYour age is {1}\nYou live in {2}".format(name, years, city)
