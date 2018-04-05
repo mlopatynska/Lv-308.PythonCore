280d1b177c4cb1aa5264927c9d77418ec21a8a99
rostikst <delfin4uk@gmail.com>
2018-04-05 14:43:08 +0300

    переписав print в .format

--- HW1/1.py
+++ HW1/1.py
@@ -1,9 +1,9 @@
-#1st HW
-name = raw_input("What is your name?\n")
-age  = raw_input("How old are you?\n")
-place_of_living = raw_input("Where do you live?\n")
+name = raw_input ( "What is your name?\n")
+age = input ( 'How old are you?\n' )
+place_of_living = raw_input ( "Where do you live?\n" )
 
-print "Hello " + name + "!",
-print "Your age is " + age + "!",
-print "You live in " + place_of_living + "!"
+# print "Hello " + name + "!",
+# print "Your age is " + age + "!",
+# print "You live in " + place_of_living + "!"
 
+print "Hello {0}! Your age is {1} and you live in {2}".format ( name, age, place_of_living )
