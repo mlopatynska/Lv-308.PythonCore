1f0141e095a8919949da19ab1590f3a4ecbfa063
Radbull123 <dmyterkotaras91@gmail.com>
2018-04-05 13:17:44 +0300

    Here we go

---
+++ HT1/Task1_new_formating.py
@@ -0,0 +1,7 @@
+name = raw_input('Enter Your name: ')
+
+age = input('How old are you? ')
+
+live = raw_input('Where are you live? ')
+
+print' Hello {}\n Your age is {}\n You live in {}'.format(name, age, live)
