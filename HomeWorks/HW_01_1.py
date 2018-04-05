e0da46a7b84d09f53aaf291980c2d6aa969bf519
Wirtos_new/Tep4ekeMI <game999888@gmail.com>
2018-04-05 15:14:29 +0300

    HW1_final

--- HomeWorks/HW_01_1.py
+++ HomeWorks/HW_01_1.py
@@ -1,9 +1,4 @@
-# encoding=utf8
-import sys
-reload(sys)
-sys.setdefaultencoding('utf8')
-n = "\n"
-name = str(unicode(raw_input("What is your name? ")))
+name = str((raw_input("What is your name? ")))
 years = str(unicode(raw_input("How old are you? ")))
 city = str(unicode(raw_input( "Where are you live? ")))
 print "Hello, {0}\nYour age is {1}\nYou live in {2}".format(name, years, city)
