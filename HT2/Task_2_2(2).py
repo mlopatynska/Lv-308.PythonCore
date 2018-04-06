6ac8807bc48a3283a86430377f2739cfff530748
Radbull123 <dmyterkotaras91@gmail.com>
2018-04-06 11:10:50 +0300

    Rewrite Task2_2.2

--- HT2/Task_2_2(2).py
+++ HT2/Task_2_2(2).py
@@ -1,16 +1,14 @@
 
-f_digit = list(raw_input("Please enter 4-digit number: "))
+f_digit = raw_input("Please enter 4-digit number: ")
 # Type any 4-digit number, that what you want.
 
-mult = int(f_digit[0]) * int(f_digit[1])\
+mult = int(f_digit[0]) * int(f_digit[1]) \
        * int(f_digit[2]) * int(f_digit[3])
 print mult
 # Multiply of this 4-digit number
 
-f_digit.reverse()
-print "".join(f_digit)
+print f_digit[::-1]
 # Returning this number from end until begin
 
-f_digit.sort()
-print "".join(f_digit)
+print ''.join(sorted(f_digit))
 # Sorting any characters in alphabet order
