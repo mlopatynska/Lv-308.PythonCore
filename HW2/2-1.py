df627db780c2416a41a46a3f9cbae1569572a2a3
Rostikst <delfin4uk@gmail.com>
2018-04-10 01:40:11 +0300

    added a part of task 2.1

--- HW2/2-1.py
+++ HW2/2-1.py
@@ -1,34 +1,33 @@
-zen_of_python = """
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
-Sparse is better than dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
-Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!\n"""
+from math import *
+x = input("Enter value x: ")
+y = input("Enter value y: ")
+z = input("Enter value z: ")
 
-print (
-    "Python phylosophy in uppercase:\n{0}\n"
-    "'&' instead of 'i':\n{1}\n"
-    "Words 'better', 'never' "
-    "and 'is' repeats here {2} {3}"
-    " and {4} times.\n".format(
-                        zen_of_python.upper(),
-                        zen_of_python.replace('i','&'),
-                        zen_of_python.count('better'),
-                        zen_of_python.count('never'),
-                        zen_of_python.count('is')))
+# TASK A
+a = (sqrt(fabs(x-1))-(fabs(y)**1/3))/(1+x**2/2+y**2/4)
+b = x*(atan(z)+exp(-(x+3)))
+print "A)\n a = {}\n b = {}".format(a, b)
 
+# TASK B
+a = (3+exp(y-1))/(1+x**2*(fabs(y-tan(z))))
+b = 1+fabs(y-x)+((y-x)**2)/2
+print "B)\n a = {}\n b = {}".format(a, b)
 
+# TASK C
+a = (1+y)*((x+y/(x**2+4))/exp(-x-2)+1/(x**2+4))
+b = (1+cos(y-2))/(x**4/2+(sin(z))**2)
+print "C)\n a = {}\n b = {}".format(a, b)
+
+#TASK D
+a = y+(x/(y**2+fabs(x**2/(y+x**3/3))))
+b = (1+(tan(z/2)**2))
+print "D)\n a = {}\n b = {}".format(a, b)
+
+#TASK E
+a = (2*cos(x-pi/6))/(1/2+(sin(y))**2)
+b = (cos(atan(1/z)))
+print "E)\n a = {}\n b = {}".format(a, b)
+
+#TASK F
+#a = x+((1+(sin(x+y))**2)/(2+fabs(x-2x/(1+x**2*y**2))))
+#print "F)\n a = {} ".format(a)
