345ff6b360764cad7e1d8025c7514681a47ad769
Wirtos_new/Tep4ekeMI <game999888@gmail.com>
2018-04-10 10:15:07 +0300

    HW_02_1

---
+++ HomeWorks/HW_02_1.py
@@ -0,0 +1,30 @@
+from math import *
+x = 12
+y = 1441
+z = 435
+answer1 = (sqrt(fabs(x - 1)) - fabs(y) ** 1 / 3.0) / (1 + (x**2 / 2)+(y ** 2 / 4))
+answer2 = (x * (atan(z) + exp(-(x + 3))))
+
+
+answer3 = (3 + exp(y - 1)) / (1 + x**2 * fabs(y - tan(z)))
+answer4 = 1 + fabs(y - x) + (y - x)**2/2 + (fabs(y - x))**3/3
+
+
+answer5 = (1 + y)*(x + y/(x**2 + 4)) / (exp(-x - 2) + 1/(x**2 + 4))
+answer6 = (1 + cos(y - 2))/(x**4/2 + (sin(z))**2)
+
+
+answer7 = y + x / (y ** 2 + fabs(x**2/(y + x**3/3)))
+answer8 = 1 + (tan(z / 2) ** 2)
+
+
+answer9 = (2 * cos(x - pi / 6)) / (1.0/2 + (sin(y))**2)
+answer10 = 1 + z**2/(3 + z**2/5)
+
+
+answer11 = (1 + (sin(x + y))**2) / (2 + fabs(x - 2*x/(1 + x**2 * y**2))) + x
+answer12 = (cos(atan(1/z)))**2
+
+
+answer13 = log(fabs((y - sqrt(fabs(x))) * (x - y/(z + x**2/4))))
+answer14 = x - x**2 / factorial(3) + x**5 / factorial(5)
