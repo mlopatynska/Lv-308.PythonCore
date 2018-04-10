879eabab7a10388790d3e721700a447b98d1ec59
Wirtos_new/Tep4ekeMI <game999888@gmail.com>
2018-04-10 10:31:35 +0300

    bonus_fixed

--- HomeWorks/bonus.py
+++ HomeWorks/bonus.py
@@ -1,29 +1,34 @@
-import copy, random
-test_list = []
-for _ in range(0, 3):
-    test_list.append(str(random.randrange(100)))
+import copy, random, re
+test_list = str(input("Print 3 str: "))
+
+# making full new copy of list
 new_list = copy.deepcopy(test_list)
-print(id(test_list))
+print("First list id",id(test_list))
 print(test_list)
-print(id(new_list))
+print("Second list id",id(new_list))
 print(new_list)
-one, two, three = test_list
-long_list = []
-for _ in range(0, random.randrange(4,30)):
-    long_list.append(str(random.randrange(100)))
+one, two, three = test_list[0], test_list[1], test_list[2]
+print(one, two, three)
+long_list = str(input("Print ∞ str"))
+
+# getting 1/3 len of the list
 partslist = int(len(long_list)/3)
+
 listone = long_list[:partslist]
+
 listtwo = long_list[partslist:partslist+partslist]
+
 listthree = long_list[partslist+partslist:]
-onelong, twolong, threelong = listone,listtwo, listthree
+
+onelong, twolong, threelong = listone, listtwo, listthree
+
 print(onelong, twolong, threelong)
 rad = int(input("Write your radius: "))
 print((rad-1)**2 + rad**2)
-pandigit = str(input("Write yor digits: "))
-if "1" and "2" and "3" and "4" and "5" and "6" and "7" and "8" and "9" and "0" in pandigit:
-    print("True")
-else:
-    print("False")
+is_pandigit = sorted(str(input("Write yor digits: ")))
+pandigit = sorted(str(1234567890))
+# IDK, how to do it properly without IF statement
+print(len(pandigit) == len(list(set(is_pandigit).intersection(set(pandigit)))))
 shift = int(input("Write your n number: "))
 circle_list = [5, 6, 7, 8, 9, 0, 1, 2, 3, 4]
 print(circle_list[shift])
