0ba9df5705bd100088d57acbcb93aa397b992974
Wirtos <game999888@gmail.com>
2018-04-10 20:52:46 +0300

    Bonus_fixed

--- HomeWorks/bonus.py
+++ HomeWorks/bonus.py
@@ -9,7 +9,7 @@ print("Second list id",id(new_list))
 print(new_list)
 one, two, three = test_list[0], test_list[1], test_list[2]
 print(one, two, three)
-long_list = str(input("Print ∞ str"))
+long_list = str(input("Print ∞ str "))
 
 # getting 1/3 len of the list
 partslist = int(len(long_list)/3)
@@ -25,10 +25,9 @@ onelong, twolong, threelong = listone, listtwo, listthree
 print(onelong, twolong, threelong)
 rad = int(input("Write your radius: "))
 print((rad-1)**2 + rad**2)
-is_pandigit = sorted(str(input("Write yor digits: ")))
-pandigit = sorted(str(1234567890))
-# IDK, how to do it properly without IF statement
-print(len(pandigit) == len(list(set(is_pandigit).intersection(set(pandigit)))))
+is_pandigit = str(input("Write yor digits: "))
+
+print(len(str(1234567890)) == len(list(set(is_pandigit).intersection(set(str(1234567890))))))
 shift = int(input("Write your n number: "))
 circle_list = [5, 6, 7, 8, 9, 0, 1, 2, 3, 4]
 print(circle_list[shift])
