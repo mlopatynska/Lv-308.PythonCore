53aba59b6d6a2fba720a60696c9282ec1b81d54b
Rostikst <delfin4uk@gmail.com>
2018-04-14 23:55:14 +0300

    HW3

---
+++ HW3/HW3-2.py
@@ -0,0 +1,16 @@
+#check if datetime is valid
+
+from calendar import monthrange
+from datetime import date
+Y = int(input("Enter year:\n"))
+M = int(input("Enter month:\n"))
+D = input("Enter day: \n")
+if M in monthrange(year, month) is True:
+       print("{}-{}-{} is correct".format(Y, M, D))
+else:
+    print("{}-{}-{} is incorrect format of date".format(Y, M, D))
+print('End') """
+
+import datetime,calendar
+today = datetime.datetime
+print(today)
