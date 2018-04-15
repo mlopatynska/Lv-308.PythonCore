3579c5d5b5521ab2779f6a109d5c6109edbd19d3
Rostikst <delfin4uk@gmail.com>
2018-04-15 16:02:34 +0300

    3 tasks

--- HW2/bonus.py
+++ HW2/bonus.py
@@ -1,19 +1,22 @@
 #Copy x to y  so that id(x) != id(y), but x == y.
-x = [ 1, 2 ]
-y = x
-print (('x = {}\ny = {}\n    id(x )= {}\n\
-    id(y )={}\n').format(x, y, id(x ), id(y )))
+x = [1, 2]
+y = x[:]
+print(('x = {}\ny = {}\n    id(x)= {}\n\
+    id(y)={}\n').format(x, y, id(x), id(y)))
 
 
 #Transfer list value  that contain 3 elements to 3 variables
-x = [ 1, 2, 3 ]
-y  = x
-a, b, c  = y
-print ('\n a = {}\n b = {}\n c ={} '.format(a, b, c ) )
+x = [1, 2, 3]
+y = x
+a, b, c = y
+print('\n a = {}\n b = {}\n c = {} '.format(a, b, c ) )
 
 #Transfer list value of any lenght to 3 variables
+x = [1, 2, 3, 5]
 a = b = c = x
-print ('\n a = {}\n b = {}\n c ={} '.format(a, b, c ) )
+print('\n a = {}\n b = {}\n c = {} '.format(a, b, c))
+
+
 
 
 
