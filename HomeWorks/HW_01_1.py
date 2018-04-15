8a1d9a7f28b5afe107267652a264306dcbe36058
Wirtos_new/Tep4ekeMI <game999888@gmail.com>
2018-04-15 23:23:28 +0300

    Python3_ver

--- HomeWorks/HW_01_1.py
+++ HomeWorks/HW_01_1.py
@@ -1,4 +1,4 @@
-name = raw_input("What is your name? ")
-years = raw_input("How old are you? ")
-city = raw_input( "Where are you live? ")
-print "Hello, {0}\nYour age is {1}\nYou live in {2}".format(name, years, city)
+name = input("What is your name? ")
+years = input("How old are you? ")
+city = input( "Where are you live? ")
+print("Hello, {0}\nYour age is {1}\nYou live in {2}".format(name, years, city))
