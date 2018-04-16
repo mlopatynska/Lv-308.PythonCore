2f1f074c55cee87e423d47558adf658ec207efb9
unknown <domanskah7777@gmail.com>
2018-04-16 09:11:28 +0300

    Task2

--- HW_3/task2.py
+++ HW_3/task2.py
@@ -4,7 +4,7 @@ year = input("Input number of the year (A.D.)...\n")
 month = input("Input number of the month...\n")
 day = input("Input  number of the day...\n")
 
-if not isinstance(year, int) or year <= 0 or year >= 9999:
+if not(isinstance(year, int)) or year <= 0 or year >= 9999:
     print("The number for the year is incorrect")
 elif not isinstance(month, int) or month <= 0 or month >= 13:
     print("Month must be in 1..12")
