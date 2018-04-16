d7d1179f769a2015e3292ea278fd1ebc8befd3f9
rostikst <delfin4uk@gmail.com>
2018-04-16 17:30:07 +0300

    3-5

--- HW3/3-5.py
+++ HW3/3-5.py
@@ -1,21 +1,21 @@
-'''Name of weekday by day number.
-Year is not leap & first day of year is Monday'''
+"""Name of weekday by day number.
+Year is not leap & first day of year is Monday"""
 
 day_n = int(input('Enter day of year in range 1-365: '))
-week = [ 'Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday',  'Sunday' ]
+week = ('Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday', 'Saturday',  'Sunday' )
 if day_n > 365 or day_n <= 0:
     print(' Invalid date! ')
 elif(day_n % 7 )== 0:
-    print(f' This day of year is {week[0]}')
-elif (day_n % 7 )== 1:
-    print(f' This day of year is {week[1]}')
-elif (day_n % 7 )== 2:
-    print(f' This day of year is {week[2]}')
-elif (day_n % 7 )== 3:
-    print(f' This day of year is {week[3]}')
-elif (day_n % 7 )== 4:
-    print(f' This day of year is {week[4]}')
-elif (day_n % 7 )== 5:
-    print(f' This day of year is {week[5]}')  
-elif (day_n % 7 )== 6:
-    print(f' This day of year is {week[6]}')
+    print(f'This day of year is {week[0]}')
+elif (day_n % 7 ) == 1:
+    print(f'This day of year is {week[1]}')
+elif (day_n % 7 ) == 2:
+    print(f'This day of year is {week[2]}')
+elif (day_n % 7 ) == 3:
+    print(f'This day of year is {week[3]}')
+elif (day_n % 7 ) == 4:
+    print(f'This day of year is {week[4]}')
+elif (day_n % 7 ) == 5:
+    print(f'This day of year is {week[5]}')
+elif (day_n % 7 ) == 6:
+    print(f'This day of year is {week[6]}')
