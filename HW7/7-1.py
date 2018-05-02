f9b056c8aa9e712d1013543dc5abfbc0414d4833
rostikst <delfin4uk@gmail.com>
2018-05-02 16:49:23 +0300

    7-1 bubble's sort

--- HW7/7-1.py
+++ HW7/7-1.py
@@ -1,14 +1,16 @@
 #7-1
-def bubble_sort(A):
+def bubble(a):
     """function for sorting arrays by bubble_sort method"""
 
-    last_el = len(A)-1
-    for i in range(0, last_el):
-        for x in range(0, last_el - i):
-            if A[x] < A[x+1]:
-                A[x+1], A[x] = A[x], A[x+1]
-    return A
+    last_el = len(a)-1
+    for i in range(last_el):
+        for x in range(last_el - i):
+            if a[x] > a[x+1]:
+                a[x+1], a[x] = a[x], a[x+1]
+    return a
 
 
-my_list = [23, 12, 9, 16, 8, 7]
-print(bubble_sort(my_list))
+my_list = [16, 12, 8, 5, 3, 15, 14, 7]
+print(f"Unsorted list:", my_list)
+print(f"\nSorted by bubble's method:", bubble(my_list))
+bubble(my_list)
