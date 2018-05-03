461eee2440afc7415f389d790d52ebc95bc6bc6b
Radbull123 <dmyterkotaras91@gmail.com>
2018-05-03 17:34:03 +0300

    linear search

---
+++ HT7/linear_search.py
@@ -0,0 +1,10 @@
+
+def linear_search(ex_list, find):
+    for x in ex_list:
+        if x == find:
+            return True
+
+    return False
+
+
+a = [44, 20, 13, 5, 13, 8, 13, 1, 0, 0, 1, 2, 7, 6, 3]
